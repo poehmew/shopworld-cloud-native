53953520661e416011566344d8b4d8f3f40065b3
poehmew <poehmew@gmail.com>
2026-06-17 15:13:20 +0200

    Add Google provider configuration in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+    }
+  }
+}
 
+provider "google" {
+  project = "shopworld-demo"
+  region  = "europe-west1"
+}
