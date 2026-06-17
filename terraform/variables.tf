ba073db1da427cc2c559b09d3fb6e2ac6788397a
poehmew <poehmew@gmail.com>
2026-06-17 15:26:29 +0200

    Add project_id and region variables for Terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "Google Cloud project ID"
+  type        = string
+  default     = "shopworld-demo"
+}
+
+variable "region" {
+  description = "Google Cloud region"
+  type        = string
+  default     = "europe-west1"
+}
